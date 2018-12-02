2b11029e1d427b435d14af4e29a246b32a975cfd
DefRect <43281438+DefRect@users.noreply.github.com>
2018-12-02 21:20:51 +0300

    Update Lab10.c

--- Lab10.c
+++ Lab10.c
@@ -1,4 +1,3 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,15 +6,15 @@
 
 int main()
 {
-	char **ptr = NULL;
-	char prev_c = ' ';
-	char c;
+	char **ptr = NULL; 
+	char prev_c = ' '; 
+	char c; 
 	int count_p = 0;
-	int i = 0, j, tmp = 0, count;
-	int flag = 1;
+	int i = 0, j = 0; 
+	int flag = 0; 
 
-	FILE *fl_in = fopen("Text.txt", "r");
-	if (fl_in == NULL)
+	FILE *fl_in = fopen("Text.txt","r"); 
+	if(fl_in == NULL) 
 	{
 		printf("ERROR");
 		system("pause");
@@ -23,51 +22,43 @@ int main()
 	}
 	setlocale(LC_ALL, "Rus");
 
-	while (!feof(fl_in))
+	while(!feof(fl_in))
 	{
-		count = 0;
 		c = fgetc(fl_in);
-		tmp++;
-		if ((c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9') && (prev_c = ' '))
+		if(((c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')) && flag == 0)
 		{
-			while (1)
+			count_p++;
+			ptr = (char**)realloc(ptr, count_p * sizeof(char*));
+			ptr[i] = (char*)malloc(0);
+			j = 0;
+			flag = 1;
+			while(1)
 			{
-				count++;
-				prev_c = c;
-				c = fgetc(fl_in);
-				tmp++;
-				if (((c == ' ') || (c == -1)) && (prev_c == '.' || prev_c == '?' || prev_c == '!'))
+				if(((c < 'A' || c > 'Z') && (c < '0' || c > '9') && (c < 'a' || c > 'z')) && (prev_c == '.' || prev_c == '?' || prev_c == '!'))
 				{
-					count_p++;
-					ptr = (char**)realloc(ptr, count_p * sizeof(char*));
-					ptr[i] = (char*)malloc(count);
-					fseek(fl_in, tmp - count - 1, SEEK_SET);
-					fread(ptr[i], sizeof(char), count, fl_in);
-					c = fgetc(fl_in);
+					ptr[i] = (char*)realloc(ptr[i], (j + 1)*sizeof(char));
+					ptr[i][j] = '\0';
+					flag = 0;
 					i++;
 					break;
 				}
+				ptr[i] = (char*)realloc(ptr[i], (j + 1)*sizeof(char));
+				ptr[i][j] = c;
+				prev_c = c;
+				c = fgetc(fl_in);
+				j++;
 			}
 		}
 	}
 
-	printf("Количество предложений: %d\n\n", count_p);
-	for (i = 0; i < count_p; i++)
-	{
-		printf("Предложение %d: ", i + 1);
-		j = 0;
-		while (ptr[i][j] != 'э')
-		{
-			printf("%c", ptr[i][j]);
-			j++;
-		}
-		printf("\n\n");
-	}
+	printf("Количество предложений: %d\n\n", count_p); 
+	for(i = 0; i < count_p; i++)
+		printf("Предложение %d: %s\n\n", i + 1, ptr[i]);
 
-	for (i = 0; i < count_p; i++)
+	for(i = 0; i < count_p; i++)
 		free(ptr[i]);
 
-	fclose(fl_in);
-	free(ptr);
+	fclose(fl_in); 
+	free(ptr); 
 	system("pause");
 }
